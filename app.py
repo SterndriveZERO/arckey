ee9045e48482f7526ad7554576b18f0a6e49f685
stern <sterndrivezero@gmail.com>
2026-05-20 17:33:18 -0400

    update

--- app.py
+++ app.py
@@ -4,16 +4,20 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("home.html")
+    return render_template("intro.html")
 
 
 @app.route("/shop")
 def shop():
-    return render_template("index.html")
+    return render_template("shop.html")
 
 @app.route("/home")
 def main():
     return render_template("home.html")
+
+@app.route('/products/arckey')
+def arckey():
+    return render_template('arckey.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
